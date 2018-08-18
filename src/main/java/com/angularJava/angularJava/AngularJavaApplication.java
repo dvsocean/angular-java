2ba216203fe69febdcd1a831ce836743904b9078
Ocean <dvsocean@icloud.com>
2018-08-18 15:55:36 -0700

    merging two apps

---
+++ src/main/java/com/angularJava/angularJava/AngularJavaApplication.java
@@ -0,0 +1,12 @@
+package com.angularJava.angularJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularJavaApplication.class, args);
+	}
+}
